1c3d1e13fec273f5c88f0b01442f795424a4b67f
Andrew Plonski <agplonski@hotmail.com>
2012-09-15 15:21:00 -0500

    big changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Blog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.default_url_options = {:host => 'secure-ocean-9010.heroku.com'}
   # Code is not reloaded between requests
   config.cache_classes = true
   config.assets.debug = true
@@ -16,7 +16,7 @@ Blog::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
