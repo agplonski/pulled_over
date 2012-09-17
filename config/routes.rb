dfe7d893975b2d70bfb2bd0cf75b73b2a7bc4456
Andrew Plonski <agplonski@hotmail.com>
2012-09-16 21:44:45 -0500

    fds

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,6 @@ Blog::Application.routes.draw do
   
 
 root :to => "home#index"
-devise_for :users
-devise_scope :user do
-    get "sign_in", :to => "devise/sessions#new"
-    get "sign_out", :to => "devise/sessions#destroy"
-    get "sign_up", :to => "devise/registrations#new"
-  resources :posts 
-  resources :comments 
- end
 
  
 
